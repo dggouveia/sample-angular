a842a439b3f19f190529811e1598763f11bcf048
Douglas Gabriel <douglasxgabriel@gmail.com>
2016-04-29 20:45:26 -0300

    exibindo informações obrigatórias de autor de fotos

--- js/app.js
+++ js/app.js
@@ -1,2 +1,9 @@
 // Iniciando a aplicação angular
 var app = angular.module('myApp', ['ngMaterial', 'ngRoute']);
+
+// Filtro para retornar código HTML a partir de variável do controlador
+app.filter("htmlCode", ['$sce', function ($sce){
+  return function (htmlCode){
+    return $sce.trustAsHtml(htmlCode);
+  }
+}])
--- js/controllers/MainController.js
+++ js/controllers/MainController.js
@@ -1,5 +1,6 @@
 // Variáveis globais (sem acesso no documento HTML no escopo deste controlador)
 var imgElement = $('#cardImage');
+var progressBar = $("#progressBar");
 
 // Instanciando o controlador
 angular.module('myApp').controller('MainController', [
@@ -8,62 +9,92 @@ angular.module('myApp').controller('MainController', [
     $scope.message = "Digite um CEP para iniciar sua busca",
     $scope.query = "",
     $scope.imagePath = "img/index-bg.jpg",
+    $scope.imageAuthor = "",
     // Criando método de busca
     $scope.search = function (data) {
+      progressBar.removeClass('hide');
       imgElement.addClass('hide');
+      var request = {};
       // Buscando CEP pelo IBGE
-      $http.get("https://viacep.com.br/ws/" + $scope.CEP + "/json/")
+      $http.get("https://viacep.com.br/ws/" + $scope.query + "/json/")
       .then(
         // Função caso a requisição seja bem sucedida
         function (data) {
           request = {
             query: data.data.localidade
           }
-          service.textSearch(request, callback);
-          $scope.message = "Você pesquisou por " + data.data.localidade
+          googlePlacesService.textSearch(request, handleResults);
         },
-        // Função caso a requisição seja má sucedida
+        // Função caso a requisição seja má sucedida (busca no google)
         function (data) {
-
-          // Setando a o valor do campo do formulário à busca
+          // Setando o valor do campo do formulário à busca
           request = {
             query: $scope.query
           }
-          // Fazendo a busca com o valor do formulário
-          googlePlacesService.textSearch(request, function (results, status) {
-            // Verificando se há locais que correspondem à busca
-            if (status == google.maps.places.PlacesServiceStatus.OK) {
-              // Iterando sobre os locais correspondentes
-              for (var i = 0; i < results.length; i++) {
-                // recuperando o local
-                var place = results[i];
-                console.log(place);
-                // Desenhando o mapa com as informações do local
-                createMarker(place);
-                googlePlacesService.getDetails(
-                  {
-                    placeId: place.place_id
-                  }
-                  , handlePlaceDetails
-                );
+          googlePlacesService.textSearch(request, handleResults);
+        });
+      };
+
+      // Método que desenha mapa
+      function createMarker(place) {
+        var placeLoc = place.geometry.location;
+
+        map = new google.maps.Map(document.getElementById('map'), {
+          center: placeLoc,
+          zoom: 15
+        });
+      }
+
+      // Trata os resultados da busca no Google Places
+      function handleResults (results, status){
+        // Verificando se há locais que correspondem à busca
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+          // Iterando sobre os locais correspondentes
+          for (var i = 0; i < results.length; i++) {
+            // recuperando o local
+            var place = results[i];
+            $scope.message = place.formatted_address;
+            // Desenhando o mapa com as informações do local
+            createMarker(place);
+            googlePlacesService.getDetails(
+              {
+                placeId: place.place_id
               }
-            }
-          });
-          $scope.message = "Realizar nova busca"
+              , handlePlaceDetails
+            );
+          }
         }
-      );
-    };
-    // Função para receber resultado da busca detalhada no google places
-    function handlePlaceDetails(data, status) {
-      if (status == google.maps.places.PlacesServiceStatus.OK) {
-        console.log(data);
-        $scope.imagePath = data.photos[Math.floor((Math.random() * 10 % data.photos.length))].getUrl({'maxWidth': 500, 'maxHeight': 300});
-        $scope.message = data.formatted_address;
-        imgElement.on('load',function(){imgElement.removeClass('hide')})
-      } else {
-        console.log("erro")
-        console.log(data);
+      };
+      // Função para receber resultado da busca detalhada no google places
+      function handlePlaceDetails(data, status) {
+        // Recomendação do Google para que trate o status da resposta
+        // Mais detalhes
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+          console.log(data);
+          try{
+            // Setando uma imagem aleatório para card
+            photo = data.photos[Math.floor((Math.random() * 10 % data.photos.length))];
+            $scope.imagePath = photo.getUrl({'maxWidth': 500, 'maxHeight': 300});
+            $scope.imageAuthor = photo.html_attributions
+            // Método para atualizar a visão
+            // Normalmente não é necessário, porém, como acontecem muitas chamadas assíncronas
+            // Faz-se necessário informar o Angular quando será necessário atualizar
+            // Mais detalhes em: http://jimhoskins.com/2012/12/17/angularjs-and-apply.html
+            $scope.$apply();
+            // Quando a imagem carregar, torna-a vizível
+            imgElement.find('img').on('load',function(){
+              imgElement.removeClass('hide');
+            })
+          }catch (err){
+            // Caso o local não possua imagens, uma execeção será lançada
+            console.log('O local não dispõe de imagens');
+          }finally{
+            progressBar.addClass('hide');
+          }
+        } else {
+          console.log("erro")
+          console.log(data);
+        }
       }
     }
-  }
-]);
+  ]);
